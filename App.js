cc0c191ed9b68f4a60139d190160ba450cff2fde
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-10-19 14:11:17 -0300

    custom fonts (Nunito-Bold & Nunito-Regular)

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import * as Font from 'expo-font';
 import Home from './screens/home';
+import AppLoading from 'expo-app-loading';
+
+const getFonts = () => Font.loadAsync({
+  'Nunito-Regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'Nunito-Bold': require('./assets/fonts/Nunito-Bold.ttf')
+});
 
 export default function App() {
-  return (
-    <Home />
-  );
+
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if(fontsLoaded) {
+    return (
+      <Home />
+    );
+  } else {
+    return (
+      <AppLoading 
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={() => console.log('error')}
+      />
+    );
+  }
+
 }
 
 
