abec37bb06471725fd4f7ccc9ee2bc17b2e3dd9b
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-10-20 14:01:23 -0300

    Revert "Global Styles"
    This reverts commit d5814e8c358ee2746500303e86767d639c17c9b0.

--- screens/home.js
+++ screens/home.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import {globalStyles} from '../styles/global';
 
 export default function Home() {
     return(
-        <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home Screen</Text>
+        <View style={styles.container}>
+            <Text style={styles.titleText}>Home Screen</Text>
         </View>
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+    },
+    titleText: {
+        fontFamily: 'Nunito-Bold',
+        fontSize: 18,
+    }
+});
