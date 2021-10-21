264bc4b704b482a12cb8e410ab8183fb589a6272
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-10-21 14:01:09 -0300

    Navigation Options (title) & defaultNavigationsOptions (headerStyle & headerTintColor)

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -5,14 +5,34 @@ import ReviewDetails from '../screens/reviewDetails';
 
 const screens = {
     Home: {
-        screen: Home
+        screen: Home,
+        navigationOptions: {
+            title: 'GameZone',
+            // headerStyle: {
+            //     backgroundColor: '#EEE'
+            // }
+        }
     },
     ReviewDetails: {
-        screen: ReviewDetails
+        screen: ReviewDetails,
+        navigationOptions: {
+            title: 'Review Details',
+            // headerStyle: {
+            //     backgroundColor: '#EEE'
+            // }
+        }
     },
 
 };
 
-const HomeStack= createStackNavigator(screens);
+const HomeStack= createStackNavigator(screens, {
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: '#EEE',
+            height: 80,
+        },
+        headerTintColor: '#444',
+    }
+});
 
 export default createAppContainer(HomeStack);
