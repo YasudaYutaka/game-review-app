e43fee30121b22212ee5868b1a52577d8c1d3158
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-10-21 13:50:39 -0300

    sending data from home.js to reviewDetails.js

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -6,7 +6,9 @@ export default function ReviewDetails({navigation}) {
 
     return(
         <View style={globalStyles.container}>
-            <Text>ReviewDetails Screen</Text>
+            <Text>{navigation.getParam('title')}</Text>
+            <Text>{navigation.getParam('body')}</Text>
+            <Text>{navigation.getParam('rating')}</Text>
         </View>
     );
 }
