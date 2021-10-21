eacf92daee15a10464692ad0008843e4dc59bbd2
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-10-21 13:43:08 -0300

    pressHandler() & Button removed

--- screens/home.js
+++ screens/home.js
@@ -4,15 +4,9 @@ import {globalStyles} from '../styles/global';
 
 export default function Home({navigation}) {
 
-    const pressHandler = () => {
-        //navigation.navigate('ReviewDetails');
-        navigation.push('ReviewDetails');
-    }
-
     return(
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home Screen</Text>
-            <Button title='Go to review dets' onPress={pressHandler} />
         </View>
     );
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -4,14 +4,9 @@ import {globalStyles} from '../styles/global';
 
 export default function ReviewDetails({navigation}) {
 
-    const pressHandler = () => {
-        navigation.goBack();
-    }
-
     return(
         <View style={globalStyles.container}>
             <Text>ReviewDetails Screen</Text>
-            <Button title='back to home screeen' onPress={pressHandler} />
         </View>
     );
 }
